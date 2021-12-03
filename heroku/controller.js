7d024df07ad93da6455cc5b6996c3922f998f99c
Filmptz <parichaya35@gmail.com>
2021-12-03 17:07:04 +0700

    update response

--- heroku/controller.js
+++ heroku/controller.js
@@ -1,6 +1,7 @@
 const EventEmitter = require('eventemitter3')
 const express = require('express')
 const router = express.Router()
+const { FB } = require('fb/lib/fb')
 
 const emitter = new EventEmitter()
 
@@ -12,16 +13,21 @@ router
 			'Cache-Control': 'no-cache'
 		})
 		res.write('Start Streaming Message\n\n')
-
-		const onMessage = (data) => {
+ 
+		FB.setAccessToken('EAACZCmYt8wZBEBAKaorrN8Bo8WN4CXBOOLBEEI9M1MDBBo2JHbTChTPrPfrP6eP5RwbHO6pfQLFMUwnarW6Gt6eIhTKfZAPunwbscHo1V5gG7XKtyh1kA2yismT6SZAN6a7UPtU1xhgAdLLDKb0NNK4X3MoKWSTAWhEc4QZA3uHaK4hvNFxxPGVFrPClDFWAZD')
+		const onMessage = async (data) => {
 			res.write(
 				`data: ${JSON.stringify({
-					data
-					// sender: data.entry[0].messaging[0].sender.id,
-					// text: data.entry[0].messaging[0].message.text
-					// 	? data.entry[0].messaging[0].message.text
-					// 	: false,
-					// timestamp: data.entry[0].messaging[0].timestamp
+					sender: {
+						ig_id: data.entry[0].messaging[0].sender.id,
+						profile_url: (await FB.api(`${data.entry[0].messaging[0].sender.id}`)).profile_pic
+						? (await FB.api(`${data.entry[0].messaging[0].sender.id}`)).profile_pic
+						: ''
+					},
+					text: data.entry[0].messaging[0].message.text
+						? data.entry[0].messaging[0].message.text
+						: false,
+					timestamp: data.entry[0].messaging[0].timestamp
 				})}\n\n`
 			)
 		}
